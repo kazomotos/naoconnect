19196f42cc97bcee6081df1e01eb6621b4d3d621
Rupert Wieser <rupertwieser@MacBook-Pro-von-Rupert.local>
2022-03-24 20:52:06 +0100

    new mqtt client

--- listeners/mqtt.py
+++ listeners/mqtt.py
@@ -1,80 +1,73 @@
+from http import client
 from naoconnect.TinyDb import TinyDb
 from naoconnect.Param import Param
 from datetime import datetime
 from time import sleep
 from threading import Thread
-import paho.mqtt.subscribe as subscribe
-
-
+from paho.mqtt.client import Client
 
 class Mqtt(Param):
     SECTONANO = 1000000000
 
     def __init__ (self, broker, tiny_db_name="mqtt.json", error_log=False):
         self.db = TinyDb(tiny_db_name)
+        self.Client = Client()
+        self.Client.on_message = self.__on_message
+        self.Client.on_connect = self.__on_connect
+        self.Client.on_disconnect = self.__on_disconnect
         self.broker = broker
         self.transfere = self._getTransferChannels()
         self.data = []
         self.add_data = self.data.append
-        self.disconnect = False
-        self.disconnected = True
         self.error_log=error_log
-    
+        self._connect()
+
     def getTelegrafData(self):
         data = self.data
         self.data = []
         self.add_data = self.data.append
         return(data)
+
+    def refreshConnection(self):
+        self.print("refresh mqtt")
+        try:
+            self.Client.loop_stop()
+        except:
+            None
+        self.Client.disconnect()
+        self._connect()
     
+    def exit(self):
+        self.Client.disconnect()
+        return(-1)        
+
     def _connect(self):
-        Thread(target=self.__connect, args=()).start()
+        self.Client.connect(host=self.broker)
+        self._subscribe()
+        Thread(target=self.Client.loop_start, args=()).start()
 
-    def __connect(self):
-        self.disconnected = False
-        subscribe.callback(
-            self.__on_message, 
-            list(self.transfere.keys()), 
-            hostname=self.broker
-        )
+    def _subscribe(self):
+        for channel in self.transfere:
+            self.Client.subscribe(channel)
+
+    def __on_connect(self, client, userdata, flags, rc):
         self.print("mqtt connected")
+    
+    def __on_disconnect(self, client, userdata, rc):
+        self.print("mqtt disconnect")
 
-    def __on_message(self, client, userdata, message):
+    def __on_message(self, client, userdata, msg):
         try:
             self.add_data(Mqtt.FORMAT_TELEGRAFFRAMESTRUCT % (
-                    self.transfere[message.topic][0], 
-                    self.transfere[message.topic][1], 
-                    self.transfere[message.topic][2],
-                    float(message.payload),
+                    self.transfere[msg.topic][0], 
+                    self.transfere[msg.topic][1], 
+                    self.transfere[msg.topic][2],
+                    float(msg.payload),
                     int(round(datetime.timestamp(datetime.utcnow()),0)*Mqtt.SECTONANO)
             ))
         except Exception as e:
             self.print(e)
-            client.disconnect()
-            self.disconnected = True
-            self.print("mqtt disconneted")
             self.refreshConnection()
-        if self.disconnect:
-            client.disconnect()
-            self.print("mqtt disconneted")
-            self.disconnected = True
-
-    def refreshConnection(self):
-        self.print("refresh mqtt")
-        self.disconnect = True
-        while 1==1:
-            sleep(5)
-            if self.disconnected == True:
-                break
-        self.disconnect = False
-        self._connect()        
-    
-    def exit(self):
-        self.disconnect = True
-        while 1==1:
-            sleep(10)
-            if self.disconnected == True:
-                break
-        return(-1)        
 
     def _getTransferChannels(self):
         ''' 
