7003510bf75e4fc103be5ec80d628944e5396362
RupertWieser <rupert.wieser@naotilus.de>
2022-10-04 16:29:29 +0200

    fix mqtt

--- listeners/mqtt.py
+++ listeners/mqtt.py
@@ -86,7 +86,7 @@ class Mqtt(Param):
             else:
                 payload = loads(msg.payload)
                 value = payload[self.value_name]
-                timestamp = int(datetime.fromisocalendar(payload[self.timestamp_name]).timestamp()*Mqtt.SECTONANO)
+                timestamp = int(datetime.fromisoformat(payload[self.timestamp_name]).timestamp()*Mqtt.SECTONANO)
             self.add_data(Mqtt.FORMAT_TELEGRAFFRAMESTRUCT % (
                     self.transfere[msg.topic][0], 
                     self.transfere[msg.topic][1], 
